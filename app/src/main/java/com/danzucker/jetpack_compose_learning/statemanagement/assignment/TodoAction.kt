375bbd4389379978ea80a78af4bedfcd748d7bcf
Daniel Ogunleye <danielayodeji97@gmail.com>
2025-03-20 15:11:53 +0000

    created an in memory

--- app/src/main/java/com/danzucker/jetpack_compose_learning/statemanagement/assignment/TodoAction.kt
+++ app/src/main/java/com/danzucker/jetpack_compose_learning/statemanagement/assignment/TodoAction.kt
@@ -1,5 +1,9 @@
 package com.danzucker.jetpack_compose_learning.statemanagement.assignment
 
 sealed interface TodoAction {
-    data object OnCheckboxClicked : TodoAction
+    data class OnCheckboxClicked(val id: Int, val isChecked: Boolean) : TodoAction
+    data class OnDeleteIconClicked(val id: Int) : TodoAction
+    data class OnTitleTextChange(val title: String) : TodoAction
+    data class OnDescriptionTextChange(val description: String) : TodoAction
+    data object OnAddTodoClicked : TodoAction
 }

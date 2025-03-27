6f6c37fb16c03ad0abf64810a492859629b0ca5a
Daniel Ogunleye <danielayodeji97@gmail.com>
2025-03-27 18:35:54 +0000

    custom lazy layout

---
+++ app/src/main/java/com/danzucker/jetpack_compose_learning/advancelayout/LazyMindMap.kt
@@ -0,0 +1,147 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.danzucker.jetpack_compose_learning.advancelayout
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.draggable2D
+import androidx.compose.foundation.gestures.rememberDraggable2DState
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.layout.LazyLayout
+import androidx.compose.foundation.lazy.layout.LazyLayoutItemProvider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntRect
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.round
+import androidx.compose.ui.util.fastForEach
+import androidx.compose.ui.util.fastMapIndexedNotNull
+import com.danzucker.jetpack_compose_learning.ui.theme.Jetpack_Compose_LearningTheme
+import kotlin.math.roundToInt
+
+data class MindMapItem(
+    val title: String,
+    val percentageOffset: Offset
+)
+
+private data class ProcessedMindMapItem(
+    val placeable: Placeable,
+    val finalXPosition: Int,
+    val finalYPosition: Int
+)
+
+
+@Composable
+fun LazyMindMap(
+    items: List<MindMapItem>,
+    mindMapOffset: IntOffset = IntOffset.Zero,
+    onDrag: (delta: IntOffset) -> Unit,
+    itemModifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
+) {
+
+    LazyLayout(
+        modifier = Modifier
+            .draggable2D(
+                state = rememberDraggable2DState { delta ->
+                    onDrag(delta.round())
+                }
+            ),
+        itemProvider = {
+            object : LazyLayoutItemProvider {
+                override val itemCount: Int
+                    get() = items.size
+
+                @Composable
+                override fun Item(index: Int, key: Any) {
+                    Text(
+                        text = items[index].title,
+                        textAlign = TextAlign.Center,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 2,
+                        modifier = Modifier
+                            .widthIn(min = 50.dp, max = 150.dp)
+                            .heightIn(min = 50.dp, max = 150.dp)
+                            .border(
+                                width = 2.dp,
+                                color = Color.LightGray
+                            )
+                            .padding(16.dp)
+                    )
+                }
+            }
+        }
+    ) { constraints ->
+        val layoutWidth = constraints.maxWidth
+        val layoutHeight = constraints.maxHeight
+
+        val visibleArea = IntRect(
+            left = 0,
+            top = 0,
+            right = layoutWidth,
+            bottom = layoutHeight
+        )
+
+        val visibleItems = items.fastMapIndexedNotNull { index, item ->
+            val finalXPosition = (item.percentageOffset.x * layoutWidth + layoutWidth / 2 + mindMapOffset.x).roundToInt()
+            val finalYPosition = (item.percentageOffset.y * layoutHeight + layoutHeight / 2 + mindMapOffset.y).roundToInt()
+
+            val maxItemWidth = 150.dp.roundToPx()
+            val maxItemHeight = 150.dp.roundToPx()
+
+            val extentedItemBounds = IntRect(
+                left = finalXPosition - maxItemWidth / 2,
+                top = finalYPosition - maxItemHeight / 2,
+                right = finalXPosition + 3 * (maxItemWidth / 2),
+                bottom = finalYPosition + 3 * (maxItemHeight / 2),
+            )
+
+            if(visibleArea.overlaps(extentedItemBounds)) {
+                val placeable = measure(
+                    index = index,
+                    constraints = Constraints()
+                ).first()
+
+                ProcessedMindMapItem(
+                    placeable = placeable,
+                    finalXPosition = finalXPosition,
+                    finalYPosition = finalYPosition,
+                )
+            } else null
+        }
+        layout(
+            constraints.maxWidth,
+            constraints.maxHeight
+        ) {
+            visibleItems.fastForEach { processedMindMapItem ->
+                processedMindMapItem.placeable.place(
+                    x = processedMindMapItem.finalXPosition - processedMindMapItem.placeable.width / 2,
+                    y = processedMindMapItem.finalYPosition - processedMindMapItem.placeable.height / 2
+                )
+            }
+        }
+    }
+}
+
+//@Preview
+//@Composable
+//private fun LazyMindMapPreview() {
+//    Jetpack_Compose_LearningTheme {
+//        LazyMindMap(
+//            mindMapOffset = 0,
+//            onDrag = {},
+//        )
+//    }
+//}

57d619e6f407a1ea6f25aed2ee8d76beaa1c8527
Daniel Ogunleye <danielayodeji97@gmail.com>
2025-04-24 17:20:25 +0100

    learning on baseline profile

---
+++ app/src/main/java/com/danzucker/jetpack_compose_learning/performanceoptimization/BaselineProfile.kt
@@ -0,0 +1,7 @@
+package com.danzucker.jetpack_compose_learning.performanceoptimization
+
+/**
+ * We have two type of compilation in android:
+ * 1. AOT (Ahead of Time) compilation - This is when the code is compiled before it is run. This is done by the Android build tools.
+ * 2. JIT (Just in Time) compilation - This is when the code is compiled while it is run. This is done by the Android runtime.
+ */
